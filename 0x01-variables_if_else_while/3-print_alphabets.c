e7904cb258c160537a2a3a7c5faf2af05d571fcd
Scarlett2345 <jay.moyi@gmail.com>
2022-09-09 14:35:59 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,44 +1,29 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 
 
 /**
 
- *main - Entry point
+ * main - This program prints the alphabet in lowercase
 
- *Return: Always 0 (Success)
+ * and then in uppercase as well.
+
+ * Return: Always 0 (Success)
 
  */
-
-
 
 int main(void)
 
 {
-	int n;
+	char ch;
 
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 
-	printf("Last digit of %i is %i and is ", n, (n % 10));
-
-
-	if ((n % 10) == 0)
-	{
-		printf("0\n");
-	}
-	else if ((n % 10) > 5)
-	{
-		printf("greater than 5\n");
-	}
-
-	else
-	{
-		printf("less than 6 and not 0\n");
-	}
-
+	putchar('\n');
 
 	return (0);
 }
